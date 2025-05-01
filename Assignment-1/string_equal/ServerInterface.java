6b9376e14fc7aac5a6b9773f5fc590bed4deaf7a
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2025-05-01 21:50:11 +0530

    Update ServerInterface.java

--- Assignment-1/string_equal/ServerInterface.java
+++ Assignment-1/string_equal/ServerInterface.java
@@ -2,4 +2,6 @@ import java.rmi.*;
 
 public interface ServerInterface extends Remote {
 	boolean isEqual(String s1, String s2) throws RemoteException;
+
+	String concat(String s1, String s2) throws RemoteException; // New method
 }
