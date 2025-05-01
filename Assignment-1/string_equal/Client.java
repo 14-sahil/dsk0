b0cfaca1daef0a6e6fc366939ad5cd13c4b54836
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2025-05-01 21:49:26 +0530

    Update Client.java

--- Assignment-1/string_equal/Client.java
+++ Assignment-1/string_equal/Client.java
@@ -5,10 +5,18 @@ public class Client {
 		try {
 			String rmiURL = "rmi://localhost/Server";
 			ServerInterface serverInterface = (ServerInterface) Naming.lookup(rmiURL);
+
 			String s1 = args[0];
 			String s2 = args[1];
+
+			// Equality check
 			boolean result = serverInterface.isEqual(s1, s2);
 			System.out.println("At Client: isEqual(" + s1 + "," + s2 + ") = " + (result ? "Yes" : "No"));
+
+			// Concatenation
+			String concatResult = serverInterface.concat(s1, s2);
+			System.out.println("At Client: concat(" + s1 + "," + s2 + ") = " + concatResult);
+
 		} catch (Exception e) {
 			System.out.println("Exception: " + e);
 		}
