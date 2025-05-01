ea736c67ddcf6050620630d2eb0ee6b497dfbbf5
Sahilkaramkar <126070964+14-sahil@users.noreply.github.com>
2025-05-01 21:49:45 +0530

    Update ServerImplementation.java

--- Assignment-1/string_equal/ServerImplementation.java
+++ Assignment-1/string_equal/ServerImplementation.java
@@ -2,10 +2,18 @@ import java.rmi.*;
 import java.rmi.server.*;
 
 public class ServerImplementation extends UnicastRemoteObject implements ServerInterface {
-	public ServerImplementation() throws RemoteException {}
+	public ServerImplementation() throws RemoteException {
+	}
+
 	public boolean isEqual(String s1, String s2) {
 		boolean result = s1.equals(s2);
-		System.out.println("At Serer: isEqual(" + s1 + "," + s2 + ") = " + (result ? "Yes" : "No"));
+		System.out.println("At Server: isEqual(" + s1 + "," + s2 + ") = " + (result ? "Yes" : "No"));
+		return result;
+	}
+
+	public String concat(String s1, String s2) {
+		String result = s1 + s2;
+		System.out.println("At Server: concat(" + s1 + "," + s2 + ") = " + result);
 		return result;
 	}
 }
